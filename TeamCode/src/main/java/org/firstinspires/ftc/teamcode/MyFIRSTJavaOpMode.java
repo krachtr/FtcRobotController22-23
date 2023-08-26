262790a275e7189ea50cd2d42e905a07359c1b20
Ryan <krachtr@gmail.com>
2023-08-26 09:32:06 -0500

    added MyFirstJavaOpMode and WebcamTest (from opencv)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    private final ElapsedTime time = new ElapsedTime();
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Elapsed Time:",time);
+            telemetry.update();
+
+        }
+    }
+
+
+}
